08608ba355282bbfae5b34587663ebc9a6e4645c
Shivang <kshivang679@gmail.com>
2025-08-29 01:43:35 +0530

    components setup and tested the working of project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "./Components/Header"
+import Main from "./Components/Main"
+export default function App(){
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
