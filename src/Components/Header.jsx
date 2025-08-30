3d09a8b0ace9b3d3a7ed9a1d6055c43baabd7ec5
Shivang <kshivang679@gmail.com>
2025-08-31 02:50:09 +0530

    removed the default list items

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react"
 export default function Header() {
     return (
         <header>
-            <h1>Chef Claude</h1>
+            <h1>Cook Mate</h1>
         </header>
     )
 }
